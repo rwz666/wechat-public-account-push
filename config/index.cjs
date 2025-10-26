495ec73949b3d0e9fd081c8a340f0b17eb7b7ee9
rwz <3116419886@qq.com>
2025-10-26 23:06:06 +0800

    Update index.cjs
    更新为正确的用户id

--- config/index.cjs
+++ config/index.cjs
@@ -21,39 +21,39 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '五州',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyVyq2JqhImcwDhfqOMDmSh0PTi4',
+      id: 'oyVyq2CitSAnqzqU02prq96FgfCY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'pr044ui8Wu07cos0MSoN1VttmjWkuBNWKl0niE7ueCk',
+      useTemplateId: 'clDAvGLw3KOtM7gCkbjv99YL0hXIQjheRnv0Y3gQUNM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-29',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: 'rwz', year: '2004', date: '04-29',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '李四', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2004', date: '04-29',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
+      // customizedDateList: [
+      //   // 在一起的日子
+      //   { keyword: 'love_day', date: '2022-09-08' },
+      //   // 结婚纪念日
+      //   { keyword: 'marry_day', date: '2022-09-09' },
+      // ],
     },
     {
       // 想要发送的人的名字
       name: '小雪',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyVyq2JqhImcwDhfqOMDmSh0PTi4',
+      id: 'oyVyq2J4-oogRIi6-uueeSo1DwjU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'pr044ui8Wu07cos0MSoN1VttmjWkuBNWKl0niE7ueCk',
+      useTemplateId: 'btAacwTUYla4xwIVLv_x4vGKPiYSzg9ob96icWIVkdg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-15',
       festivals: [
@@ -80,7 +80,7 @@ const USER_CONFIG = {
   ],
 
 
-  // todo：【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: 'TV65uYPEQZc5NUR4Q7x2OoNjjhV9omgYw7VwOFntxhI',
 
   CALLBACK_USERS: [
