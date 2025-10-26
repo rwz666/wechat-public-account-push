e6c764d161a2cce2644aa4bb0d24c501168633a4
rwz <3116419886@qq.com>
2025-10-26 23:25:43 +0800

    Update index.cjs
    修改自己推送的模板id

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyVyq2CitSAnqzqU02prq96FgfCY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'clDAvGLw3KOtM7gCkbjv99YL0hXIQjheRnv0Y3gQUNM',
+      useTemplateId: 'btAacwTUYla4xwIVLv_x4vGKPiYSzg9ob96icWIVkdg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-29',
       festivals: [
