28939cb2f8c95a6098de4e0d560b263795cb5313
rwz <3116419886@qq.com>
2026-02-20 08:08:46 +0800

    停止执行

--- config/index.cjs
+++ config/index.cjs
@@ -1,96 +1,96 @@
-/* eslint-disable */
+// /* eslint-disable */
 
-/**
- * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
- * 自行添加属性，以支持更多个性化功能
- */
-const USER_CONFIG = {
+// /**
+//  * 此项目配置为方便新人使用，已缩减至最简配置。
+//  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+//  * 自行添加属性，以支持更多个性化功能
+//  */
+// const USER_CONFIG = {
 
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wxe3b6fa59b6f1f071',
+//   // 使用微信测试号：公众号APP_ID
+//   APP_ID: 'wxe3b6fa59b6f1f071',
 
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '9c35a02e99bac22917d2031435072e2f',
+//   // 使用微信测试号：公众号APP_SECRET
+//   APP_SECRET: '9c35a02e99bac22917d2031435072e2f',
 
-  PROVINCE: '四川',
-  CITY: '成都',
+//   PROVINCE: '四川',
+//   CITY: '成都',
 
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '五州',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyVyq2CitSAnqzqU02prq96FgfCY',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'btAacwTUYla4xwIVLv_x4vGKPiYSzg9ob96icWIVkdg',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '04-29',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: 'rwz', year: '2004', date: '04-29',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '*生日', name: '小雪宝宝', year: '2005', date: '10-15',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2025', date: '09-10',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2025-10-26' },
-        // 结婚纪念日
-        // { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-    {
-      // 想要发送的人的名字
-      name: '小雪',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyVyq2J4-oogRIi6-uueeSo1DwjU',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'btAacwTUYla4xwIVLv_x4vGKPiYSzg9ob96icWIVkdg',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '10-15',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '小雪宝宝', year: '2005', date: '10-15',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        // {
-        //   type: '生日', name: '李四', year: '1996', date: '09-31',
-        // },
-        {
-          type: '节日', name: '相识纪念日', year: '2025', date: '09-10',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2025-10-26' },
-        // 结婚纪念日
-        //{ keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
+//   USERS: [
+//     {
+//       // 想要发送的人的名字
+//       name: '五州',
+//       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+//       id: 'oyVyq2CitSAnqzqU02prq96FgfCY',
+//       // 使用微信测试号：你想对他发送的模板消息的模板ID
+//       useTemplateId: 'btAacwTUYla4xwIVLv_x4vGKPiYSzg9ob96icWIVkdg',
+//       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+//       horoscopeDate: '04-29',
+//       festivals: [
+//         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+//         {
+//           type: '*生日', name: 'rwz', year: '2004', date: '04-29',
+//         },
+//         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+//         {
+//           type: '*生日', name: '小雪宝宝', year: '2005', date: '10-15',
+//         },
+//         {
+//           type: '节日', name: '相识纪念日', year: '2025', date: '09-10',
+//         },
+//       ],
+//       // 我们在一起已经有xxxx天了的配置
+//       customizedDateList: [
+//         // 在一起的日子
+//         { keyword: 'love_day', date: '2025-10-26' },
+//         // 结婚纪念日
+//         // { keyword: 'marry_day', date: '2022-09-09' },
+//       ],
+//     },
+//     {
+//       // 想要发送的人的名字
+//       name: '小雪',
+//       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+//       id: 'oyVyq2J4-oogRIi6-uueeSo1DwjU',
+//       // 使用微信测试号：你想对他发送的模板消息的模板ID
+//       useTemplateId: 'btAacwTUYla4xwIVLv_x4vGKPiYSzg9ob96icWIVkdg',
+//       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+//       horoscopeDate: '10-15',
+//       festivals: [
+//         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+//         {
+//           type: '*生日', name: '小雪宝宝', year: '2005', date: '10-15',
+//         },
+//         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+//         // {
+//         //   type: '生日', name: '李四', year: '1996', date: '09-31',
+//         // },
+//         {
+//           type: '节日', name: '相识纪念日', year: '2025', date: '09-10',
+//         },
+//       ],
+//       // 我们在一起已经有xxxx天了的配置
+//       customizedDateList: [
+//         // 在一起的日子
+//         { keyword: 'love_day', date: '2025-10-26' },
+//         // 结婚纪念日
+//         //{ keyword: 'marry_day', date: '2022-09-09' },
+//       ],
+//     },
+//   ],
 
 
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'TV65uYPEQZc5NUR4Q7x2OoNjjhV9omgYw7VwOFntxhI',
+//   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+//   CALLBACK_TEMPLATE_ID: 'TV65uYPEQZc5NUR4Q7x2OoNjjhV9omgYw7VwOFntxhI',
 
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyVyq2CitSAnqzqU02prq96FgfCY',
-    }
-  ],
+//   CALLBACK_USERS: [
+//     {
+//       name: '自己',
+//       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+//       id: 'oyVyq2CitSAnqzqU02prq96FgfCY',
+//     }
+//   ],
 
-}
+// }
 
-module.exports = USER_CONFIG
+// module.exports = USER_CONFIG
